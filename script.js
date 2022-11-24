78d929ec160363da160eceb224a018451af7cdbc
@thuocsi.thinhtq <thinh.tran@thuocsi.vn>
2022-11-24 14:33:44 +0700

    api by vercel

--- script.js
+++ script.js
@@ -1,119 +1,218 @@
-
+let userList = []
+let dataList = []
+let allUser
+let mainTable
+let calculateTable
+let users
+let payers
+let Money
+let Reason
+let AddForm
+let AddButton
+let Noti
 let data = [];
 let user = [];
 let payer = 0;
 let money = 0;
 let reason = "";
-let dataAPI = "https://my-json-vercel.vercel.app/data"
+
+const dataURI = "https://nextjs-mongo-eta.vercel.app/api/spent"
+const userURI = "https://nextjs-mongo-eta.vercel.app/api/user"
+
+const getDataList = () => {
+    fetch(dataURI)
+    .then(res => res.json())
+    .then((res) => {
+        dataList = res.data
+    })
+    .then(() => {
+        loadUser()
+        loadTable()
+        getDomElement()
+        calculate()
+    })
+}
+
+const getUserList = () => {
+    fetch(userURI)
+    .then(res => res.json())
+    .then((res)=>{
+        userList = res.data
+        getDataList()
+    })
+    .then((() => {
+        loadUser()
+        loadTable()
+        getDomElement()
+    }))
+    .catch(error => console.log(error))
+}
+getUserList()
+
+const payerChosen = document.getElementById("payer-chosen");
+const userChosen = document.getElementById("user-chosen");
+const headTable = document.getElementById("head-table");
+const headDetail = document.getElementById("detail-head");
 
 
-const allUser = document.getElementById("all-user");
-const mainTable = document.getElementById("mainTable");
-const calculateTable = document.getElementById("calculateTable");
-const users = document.querySelectorAll(".user");
-const payers = document.querySelectorAll(".payer");
-const Money = document.getElementById("money");
-const Reason = document.getElementById("reason");
-const AddForm = document.getElementById("addForm");
-const AddButton = document.getElementById("addButton");
-const Noti = document.getElementById("noti");
+const loadUser = () => {
+    headTable.innerHTML = "<th scope='col'></th>"
+    headDetail.innerHTML = "<th scope='col'>Ngày</th>"
+    payerChosen.innerHTML = ""
+    userChosen.innerHTML = ""
+    userList.map((item) => {
+        payerChosen.innerHTML += `
+            <button type="button" class="payer btn btn-outline-danger mr-3">${item.name}</button>
+        `
+        userChosen.innerHTML += `
+            <button type="button" class="user btn btn-outline-success mr-3">${item.name}</button>
+        `
+        headTable.innerHTML += `
+            <th scope="col">${item.name}</th>
+        `
+        headDetail.innerHTML += `
+            <th scope="col">${item.name}</th>
+        `
+    })
+    headTable.innerHTML += '<th scope="col">Tổng</th>'
+    headDetail.innerHTML += 
+    `<th scope="col">Tổng</th>
+    <th scope="col">Người Chi</th>
+    <th scope="col">Lý Do</th>
+    <th scope="col">Xoá</th>`
+}
+
+const getDomElement = function() {
+    allUser = document.getElementById("all-user");
+    mainTable = document.getElementById("mainTable");
+    calculateTable = document.getElementById("calculateTable");
+    users = document.querySelectorAll(".user");
+    payers = document.querySelectorAll(".payer");
+    Money = document.getElementById("money");
+    Reason = document.getElementById("reason");
+    AddForm = document.getElementById("addForm");
+    AddButton = document.getElementById("addButton");
+    Noti = document.getElementById("noti");
+    users.forEach((item, index) => {
+        item.addEventListener('click', ()=>{
+            if (item.classList.contains("active")){
+                item.classList.remove("active");
+                let temp = user.indexOf(index+1);
+                user.splice(temp, 1);
+                allUser.classList.remove("active")
+            }
+            else {
+                item.classList.add("active");
+                user.push(index+1);
+                if (isAllActive()){
+                    allUser.classList.add("active")
+                }
+            }
+        })
+        
+    })
+    
+    payers.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            payers.forEach(item2 => {
+                item2.classList.remove("active");
+            })
+            item.classList.add("active");
+            payer = index+1;
+        })
+    })
+}
+getDomElement()
 
 const loadTable = function() {
     let namePayer = "";
     mainTable.innerHTML = "";
-    for (let i = 0; i < data.length; i++){
-    switch(data[i].payer){
-        case 1: 
-            namePayer = "Thịnh";
-            break;
-        case 2: 
-            namePayer = "Đăng";
-            break;
-        case 3: 
-            namePayer = "Danh";
-            break;
-        case 4: 
-            namePayer = "Việt";
-            break;
-        case 5: 
-            namePayer = "Bảo";
-            break;
-    }
-    let everyMoney = Math.round(((data[i].money / data[i].user.length) * 100)) / 100;
-    
-    mainTable.innerHTML += `
-        <tr id="row-${i}">
-            <th scope="row">${data[i].date}</th>
-            <td>${(data[i].user.includes(1))?everyMoney:""}</td>
-            <td>${(data[i].user.includes(2))?everyMoney:""}</td>
-            <td>${(data[i].user.includes(3))?everyMoney:""}</td>
-            <td>${(data[i].user.includes(4))?everyMoney:""}</td>
-            <td>${(data[i].user.includes(5))?everyMoney:""}</td>
-            <td>${data[i].money}</td>
-            <td>${namePayer}</td>
-            <td>${data[i].reason}</td>
-            <td><button type="button" onclick=deleteRow(${i}) class="btn btn-danger">Xoá</button></td>
-        </tr>
-    `
+    for (let i = 0; i < dataList.length; i++){
+        namePayer = userList.filter(item => item.id == dataList[i].payer)[0]?.name
+        let everyMoney = Math.round(((dataList[i].money / dataList[i].user.length) * 100)) / 100;
+        
+        mainTable.innerHTML += `
+            <tr id="row-${i}">
+                <th scope="row">${dataList[i].created_time.slice(0,10)}</th>
+                <td>${(dataList[i].user.includes(1))?everyMoney:""}</td>
+                <td>${(dataList[i].user.includes(2))?everyMoney:""}</td>
+                <td>${(dataList[i].user.includes(3))?everyMoney:""}</td>
+                <td>${(dataList[i].user.includes(4))?everyMoney:""}</td>
+                <td>${(dataList[i].user.includes(5))?everyMoney:""}</td>
+                <td>${dataList[i].money}</td>
+                <td>${namePayer}</td>
+                <td>${dataList[i].reason}</td>
+                <td><button type="button" onclick=deleteRow(${i}) class="btn btn-danger">Xoá</button></td>
+            </tr>
+        `
     }
 }
 
 const calculate = async function(){
     calculateTable.innerHTML = "";
-    let totalSpends = [0,0,0,0,0];
-    let totalUses = [0,0,0,0,0];
-    let totalRefund = [0,0,0,0,0];
+    let totalSpends = {};
+    let totalUses = {};
+    let totalRefund = {};
+    userList.forEach(item => {
+        totalSpends[item.id] = 0
+        totalUses[item.id] = 0
+        totalRefund[item.id] = 0
+    })
+    
     let totalSpend = 0;
     let totalUse = 0;
 
-    data.forEach(item => {
+    dataList.forEach(item => {
         let everyMoney = Math.round(((item.money / item.user.length) * 100)) / 100;
         totalSpend += item.money;
-        totalSpends[item.payer-1] += item.money;
+        totalSpends[item.payer] += item.money;
         item.user.forEach(item2 => {
-            totalUses[item2-1] += everyMoney;
+            totalUses[item2] += everyMoney;
         })
     })
 
-    for(let i = 0; i < 5; i++){
-        totalUses[i] = Math.round(totalUses[i]);
+    for (let i in totalUses) {
         totalUse += totalUses[i];
     }
 
-    for(let i = 0; i < 5; i++){
-        totalRefund[i] = totalSpends[i] - totalUses[i];
-    }
-
-    for(let i = 0; i < 5; i++){
+    for (let i in totalSpends) {
         totalSpends[i] = Math.round(totalSpends[i]);
     }
 
-    calculateTable.innerHTML = `
+    userList.map(item => {
+        totalRefund[item.id] = totalSpends[item.id] - totalUses[item.id]
+    })
+
+    let spendRows = ""
+    let useRows = ""
+    let refundRows = ""
+    userList.map(item => {
+        spendRows += `
+            <td>${totalSpends[item.id]}</td>
+        `
+        useRows += `
+            <td>${Math.round(totalUses[item.id])}</td>
+        `
+        refundRows += `
+            <td>${Math.round(totalRefund[item.id])}</td>
+        `
+
+    })
+
+    calculateTable.innerHTML += `
     <tr>
         <th scope="row">Chi</th>
-        <td>${totalSpends[0]}</td>
-        <td>${totalSpends[1]}</td>
-        <td>${totalSpends[2]}</td>
-        <td>${totalSpends[3]}</td>
-        <td>${totalSpends[4]}</td>
+        ${spendRows}
         <td>${totalSpend}</td>
     </tr>
     <tr>
         <th scope="row">Tiêu</th>
-        <td>${Math.round(totalUses[0])}</td>
-        <td>${Math.round(totalUses[1])}</td>
-        <td>${Math.round(totalUses[2])}</td>
-        <td>${Math.round(totalUses[3])}</td>
-        <td>${Math.round(totalUses[4])}</td>
+        ${useRows}
         <td>${totalUse}</td>
     </tr>
     <tr>
         <th scope="row">Nhận Lại</th>
-        <td>${Math.round(totalRefund[0])}</td>
-        <td>${Math.round(totalRefund[1])}</td>
-        <td>${Math.round(totalRefund[2])}</td>
-        <td>${Math.round(totalRefund[3])}</td>
-        <td>${Math.round(totalRefund[4])}</td>
+        ${refundRows}
         <td></td>
     </tr>
     `
@@ -121,19 +220,6 @@ const calculate = async function(){
 
 loadTable();
 calculate();
-
-const getData = function(){
-    fetch(dataAPI)
-        .then(res => res.json())
-        .then((res)=>{
-            data = res;
-        })
-        .then(()=>{
-            loadTable();
-            calculate();
-        })
-        .catch(error => console.log(error))
-}
 
 const addData = function(data){
     let options = {
@@ -143,7 +229,7 @@ const addData = function(data){
         },
         body: JSON.stringify(data)
     };
-    fetch(dataAPI, options)
+    fetch(dataURI, options)
         .then(res => {
             res.json();
         })
@@ -155,24 +241,32 @@ const deleteData = function(id){
         method: "DELETE",
         headers: {
             'Content-Type': 'application/json'
-        }
+        },
+        body: JSON.stringify({id})
     };
-    fetch(dataAPI + '/' + id, options)
+    fetch(dataURI, options)
         .then(res => {
-            res.json();
+            
         })
         .catch(error => console.log(error))
 }
 
-getData(dataAPI);
+const isAllActive = () => {
+    for (let item of users){
+        if (!item.classList.contains("active")) {
+            return false
+        }
+    }
+    return true
+}
 
 AddButton.addEventListener('click', ()=>{
     AddForm.classList.toggle("active");
 })
 
 const deleteRow = function(index){
-    deleteData(data[index].id);
-    data.splice(index,1);
+    deleteData(dataList[index].id);
+    dataList.splice(index,1);
     loadTable();
     calculate();
 }
@@ -202,24 +296,21 @@ const addNew = function(){
         return;
     }
     Noti.classList.remove("active");
-    let id = 1;
-    if (data.length > 0){
-        id = (data[data.length-1].id + 1)
-    }
+    let id = dataList.length > 0 ? dataList[dataList.length - 1].id + 1 : 1
     let dateObj = new Date();
     let month = dateObj.getUTCMonth() + 1; //months from 1-12
     let day = dateObj.getUTCDate();
     let year = dateObj.getUTCFullYear();
-    let date = day + "/" + month + "/" + year;
+    let created_time = day + "/" + month + "/" + year;
     let temp = {
         id,
         payer,
         money,
         user,
-        date,
-        reason
+        reason,
+        created_time,
     }
-    data.push(temp);
+    dataList.push(temp);
     addData(temp);
     Money.value = "";
     Reason.value = "";
@@ -238,62 +329,21 @@ const addNew = function(){
     calculate();
 }
 
-
-
-
-users.forEach((item, index) => {
-    item.addEventListener('click', ()=>{
-        if (item.classList.contains("active")){
-            item.classList.remove("active");
-            let temp = user.indexOf(index+1);
-            user.splice(temp, 1);
-            allUser.classList.remove("active")
-        }
-        else {
-            item.classList.add("active");
-            user.push(index+1);
-            if (isAllActive()){
-                allUser.classList.add("active")
-            }
-        }
-    })
-    
-})
-
-payers.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        payers.forEach(item2 => {
-            item2.classList.remove("active");
-        })
-        item.classList.add("active");
-        payer = index+1;
-    })
-})
-
-
-
 const deleteAll = document.getElementById("deleteAll");
 deleteAll.addEventListener('click', ()=>{
     let pass = prompt("Nhập Password để xoá!", "");
     if (pass == "confirm"){
-        let i;
-        for (i in document.querySelectorAll("tr > td > button")){
-            document.querySelectorAll("tr > td > button")[0].click();
-        }
+        dataList.forEach(item => {
+            dataList = [];
+            deleteData(item.id);
+            loadTable();
+            calculate();
+        })
     }
     else {
         alert("Mật khẩu là 'confirm' bro!");
     }
 })
-
-const isAllActive = () => {
-    for (let item of users){
-        if (!item.classList.contains("active")) {
-            return false
-        }
-    }
-    return true
-}
 
 allUser.addEventListener('click', () => {
     if (!isAllActive()){
